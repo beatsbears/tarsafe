b641be0f5a26163bb07d1605f635a547f3ff4884
grayson <grayson@returntocorp.com>
2020-10-12 12:47:41 -0700

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-about = {}
-exec(open(path.join(here, 'src/__init__.py')).read(), about)
+from tarsafe import __version__
 
 with open(path.join(here, 'README.md')) as file:
     long_description = file.read()
 
 setup(
     name='tarsafe',
-    version=about['__version__'],
+    version=__version__,
     description='A safe subclass of the TarFile class for interacting with tar files. Can be used as a direct drop-in replacement for safe usage of extractall()',
     long_description=long_description,
     author='Andrew Scott',
